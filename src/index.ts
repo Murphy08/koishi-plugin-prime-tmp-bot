4f8607e82fbf4c51f9ffe75000e501b8e943cd55
LYT <1075442383@qq.com>
2025-10-14 19:05:02 +0800

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,60 @@
 import { Context, Schema } from 'koishi'
+const model = require('./database/model')
+const { MileageRankingType } = require('./util/constant')
+const tmpQuery = require('./command/tmpQuery/tmpQuery')
+const tmpServer = require('./command/tmpServer')
+const tmpBind = require('./command/tmpBind')
+const tmpTraffic = require('./command/tmpTraffic/tmpTraffic')
+const tmpPosition = require('./command/tmpPosition')
+const tmpVersion = require('./command/tmpVersion')
+const tmpDlcMap = require('./command/tmpDlcMap')
+const tmpMileageRanking = require('./command/tmpMileageRanking')
 
-export const name = 'prime-tmp-bot'
+export const name = 'tmp-bot'
+export const inject = {
+  required: ['database'],
+  optional: ['puppeteer']
+}
 
-export interface Config {}
+export interface Config {
+  baiduTranslateEnable: boolean
+  baiduTranslateAppId: string
+  baiduTranslateKey: string
+  baiduTranslateCacheEnable: boolean
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.intersect([
+  Schema.object({
+    baiduTranslateEnable: Schema.boolean().default(false).description('启用百度翻译'),
+    baiduTranslateAppId: Schema.string().description('百度翻译APP ID'),
+    baiduTranslateKey: Schema.string().description('百度翻译秘钥'),
+    baiduTranslateCacheEnable: Schema.boolean().default(false).description('启用百度翻译缓存')
+  }).description('基本配置'),
+  Schema.object({
+    queryShowAvatarEnable: Schema.boolean().default(false).description('查询指令展示头像，部分玩家的擦边头像可能导致封号'),
+    tmpTrafficType: Schema.union([
+      Schema.const(1).description('文字'),
+      Schema.const(2).description('热力图')
+    ]).default(1).description('路况信息展示方式'),
+    tmpQueryType: Schema.union([
+      Schema.const(1).description('文字'),
+      Schema.const(2).description('图片')
+    ]).default(1).description('玩家信息展示方式'),
+  }).description('指令配置'),
+])
 
-export function apply(ctx: Context, config: Config) {
-  // write your plugin here
+export function apply(ctx: Context, cfg: Config) {
+  // 初始化数据表
+  model(ctx)
+
+  // 注册指令
+  ctx.command('tmpquery <tmpId>').action(async ({ session }, tmpId) => await tmpQuery(ctx, cfg, session, tmpId))
+  ctx.command('tmpserverets').action(async () => await tmpServer(ctx))
+  ctx.command('tmpbind <tmpId>').action(async ({ session }, tmpId) => await tmpBind(ctx, cfg, session, tmpId))
+  ctx.command('tmptraffic <serverName>').action(async ({ session }, serverName) => await tmpTraffic(ctx, cfg, serverName))
+  ctx.command('tmpposition <tmpId>').action(async ({ session }, tmpId) => await tmpPosition(ctx, cfg, session, tmpId))
+  ctx.command('tmpversion').action(async () => await tmpVersion(ctx))
+  ctx.command('tmpdlcmap').action(async ({ session }) => await tmpDlcMap(ctx, session))
+  ctx.command('tmpmileageranking').action(async ({ session }) => await tmpMileageRanking(ctx, session, MileageRankingType.total))
+  ctx.command('tmptodaymileageranking').action(async ({ session }) => await tmpMileageRanking(ctx, session, MileageRankingType.today))
 }
